f52864f5b35b2cdea836c470d6e4ad12dd83a4e9
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-05-23 09:32:42 +0800

    Fix various bugs

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ import json
 
 database_path = os.environ["DATABASE_URL"]
 
+
 db = SQLAlchemy()
 
 """
---
+++ setup.sh
@@ -0,0 +1,4 @@
+export DATABASE_URL="postgresql://postgres:postgres@localhost:5432/testdb"
+export EXCITED=true
+export FLASK_APP=app.py
+flask run --reload
